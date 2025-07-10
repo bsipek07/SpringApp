1f69c13e5407bdb0f3b919a49441d538e82c828d
bsipek07 <borna.sipek@gmail.com>
2025-07-10 19:36:56 +0200

    cc2

--- Example (1)/Example/src/main/java/hr/java/spring/boot/Example/dto/HardverDTO.java
+++ Example (1)/Example/src/main/java/hr/java/spring/boot/Example/dto/HardverDTO.java
@@ -1,6 +1,9 @@
 package hr.java.spring.boot.Example.dto;
 
 import hr.java.spring.boot.Example.data.TipHardvera;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,7 +12,41 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 public class HardverDTO {
+
+    @NotBlank(message = "Naziv ne smije biti prazan")
     private String naziv;
+
+    @NotNull(message = "Cijena mora biti unesena")
+    @Positive(message = "Cijena mora biti veća od 0")
     private BigDecimal cijena;
+
+    @NotNull(message = "Tip hardvera mora biti odabran")
     private TipHardvera tip;
+
+    public HardverDTO() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+
+    public TipHardvera getTip() {
+        return tip;
+    }
+
+    public void setTip(TipHardvera tip) {
+        this.tip = tip;
+    }
 }
